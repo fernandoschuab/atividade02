49e84af8053023afeac2cd28e0f20e774f2d1ab6
Fernando Schuab <fschuab@gmail.com>
2026-04-04 21:09:54 -0300

    feat: configure SPA mode and add deployment scripts for GitHub Pages integration

--- react-router.config.js
+++ react-router.config.js
@@ -1,5 +1,4 @@
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
+  basename: "/atividade02/",
 };
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/atividade02/",
   plugins: [reactRouter(), tailwindcss()],
 });
