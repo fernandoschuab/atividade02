daad9481b6bab1657a258cd1b27e9972084a2ff6
Fernando Schuab <fschuab@gmail.com>
2026-04-04 21:16:14 -0300

    correcao localhost

--- react-router.config.js
+++ react-router.config.js
@@ -1,4 +1,4 @@
 export default {
   ssr: false,
-  basename: "/atividade02/",
+  basename: process.env.NODE_ENV === "production" ? "/atividade02/" : "/",
 };
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/atividade02/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/atividade02/" : "/",
   plugins: [reactRouter(), tailwindcss()],
-});
+}));
